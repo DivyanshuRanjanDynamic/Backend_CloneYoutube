bc3038de7e4e1a3d8f71eb4b7368eb2fb370b87a
Divyansh <divyanshuchannel2@gmail.com>
2024-10-26 08:20:57 +0530

    Update rateLimiter.middleware.js

--- src/middleware/rateLimiter.middleware.js
+++ src/middleware/rateLimiter.middleware.js
@@ -1,6 +1,6 @@
 import rateLimit from "express-rate-limit"
 
-export const  ratelimiter=rateLimit(
+export const limiter=rateLimit(
         {
             windowMs : 15 * 60 * 1000, //15 minutes,
             max:(admin)=>  // limit admin  IP to 500 requests per windowMs and  for users IP to 100 requests per windowMs. 
